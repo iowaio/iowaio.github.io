1ba82b8490f292d3a33b71325afcfd3faeb9cb28
iowaio <39103100+iowaio@users.noreply.github.com>
2018-05-08 16:34:47 -0500

    Update main.js

--- main.js
+++ main.js
@@ -7,9 +7,9 @@ var myImage = document.querySelector('img');
 myImage.onclick = function() {
     var mySrc = myImage.getAttribute('src');
     if(mySrc === 'images/firefox-icon.png') {
-      myImage.setAttribute ('src','images/firefox2.png');
+      myImage.setAttribute ('src','/firefox2.png');
     } else {
-      myImage.setAttribute ('src','images/firefox-icon.png');
+      myImage.setAttribute ('src','/firefox-icon.png');
     }
 }
 
